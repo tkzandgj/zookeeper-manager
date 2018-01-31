bd342a3416cb0960e3bd0dc4e6e49e33e5976c85
tukangzheng <tukangzheng@gomeplus.com>
2018-01-31 09:56:47 +0800

    '修改请求Handler'

--- src/main/java/com/gome/ads/zookeeper/handler/RequestChannelHandler.java
+++ src/main/java/com/gome/ads/zookeeper/handler/RequestChannelHandler.java
@@ -4,7 +4,11 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Administrator
@@ -17,9 +21,19 @@ public class RequestChannelHandler extends ChannelInboundHandlerAdapter{
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof FullHttpRequest){
             FullHttpRequest request = (FullHttpRequest)msg;
+            String uri = request.uri();
+            QueryStringDecoder queryStringDecoder = new QueryStringDecoder(uri);
 
+            Map<String, List<String>> prarams = queryStringDecoder.parameters();
+            
         }
         super.channelRead(ctx, msg);
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
     }
 
 
@@ -27,5 +41,13 @@ public class RequestChannelHandler extends ChannelInboundHandlerAdapter{
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 
         super.exceptionCaught(ctx, cause);
+    }
+
+    private String getParams(Map<String, List<String>> params, String key){
+        List<String> list = params.get(key);
+        if (list != null && list.size() > 0){
+            return list.get(0);
+        }
+        return null;
     }
 }
